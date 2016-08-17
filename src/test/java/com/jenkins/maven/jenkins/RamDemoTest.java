e7a1ddf4d3bb3b57cf581d726772bc2f8de04482
ravigumpu <gumpuravi@gmail.com>
2016-08-17 17:15:01 +1000

    Ram Demo GitHub Push Test

--- src/test/java/com/jenkins/maven/jenkins/RamDemoTest.java
+++ src/test/java/com/jenkins/maven/jenkins/RamDemoTest.java
@@ -1,5 +1,38 @@
 package com.jenkins.maven.jenkins;
 
-public class RamDemoTest {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
+/**
+ * Unit test for simple App.
+ */
+public class RamDemoTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RamDemoTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RamDemoTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }
